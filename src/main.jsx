515d0a68d28c664a84d2371beba6cc583ca5d33b
auztinwelborn <107227017+auztinwelborn@users.noreply.github.com>
2025-06-13 16:21:06 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import WorldClockDashboard from './App.jsx'
-import './index.css'
+import { StatsigClient } from '@statsig/js-client';
+import { StatsigSessionReplayPlugin } from '@statsig/session-replay';
+import { StatsigAutoCapturePlugin } from '@statsig/web-analytics';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <WorldClockDashboard />
-  </React.StrictMode>
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import WorldClockDashboard from './App.jsx';
+import './index.css';
+
+async function startApp() {
+  const statsig = new StatsigClient(
+    'client-1jKRKqONUD66OY5whX2pFDELaEnSUFfW8vB879CBN',
+    { userID: 'demo-user' },
+    {
+      plugins: [
+        new StatsigSessionReplayPlugin(),
+        new StatsigAutoCapturePlugin(),
+      ],
+    }
+  );
+
+  await statsig.initializeAsync();
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <WorldClockDashboard />
+    </React.StrictMode>
+  );
+}
+
+startApp();
