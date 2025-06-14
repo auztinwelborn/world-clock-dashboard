202e8d239f0de6d267e57844af69add4a902b51e
Austin Welborn <austinwelborn@austins-air.lan>
2025-06-13 18:20:57 -0700

    Fix render to show WorldClockDashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { StatsigProvider, useClientAsyncInit } from "@statsig/react-bindings";
-import { StatsigAutoCapturePlugin } from "@statsig/web-analytics";
-import { StatsigSessionReplayPlugin } from "@statsig/session-replay";
-
-function App() {
-  const { client } = useClientAsyncInit(
-    "client-1jKRKqgQNUDG6QY5wHhX2pFDELaEnSUFWw8vB879CBN",
-    { userID: "a-user" }, // <-- ✅ comma added here
-    {
-      plugins: [new StatsigAutoCapturePlugin(), new StatsigSessionReplayPlugin()],
-    }
-  );
-
-  return (
-    <StatsigProvider client={client} loadingComponent={<div>Loading...</div>}>
-      <div>Hello World</div>
-    </StatsigProvider>
-  );
-}
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
