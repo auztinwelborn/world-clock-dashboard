1ebdbb6fc48b61c20f5249829623a68a67ff7120
Austin Welborn <austinwelborn@austins-air.lan>
2025-06-13 17:39:49 -0700

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { StatsigProvider, useClientAsyncInit } from "@statsig/react-bindings";
+import { StatsigAutoCapturePlugin } from "@statsig/web-analytics";
+import { StatsigSessionReplayPlugin } from "@statsig/session-replay";
 
-import { StatsigProvider, useClientAsyncInit } from '@statsig/react-bindings';
-import { StatsigAutoCapturePlugin } from '@statsig/web-analytics';
-import { StatsigSessionReplayPlugin } from '@statsig/session-replay';
-
-function StatsigWrapper() {
+function App() {
   const { client } = useClientAsyncInit(
     "client-1jKRKqgQNUDG6QY5wHhX2pFDELaEnSUFWw8vB879CBN",
-    { userID: 'a-user' },
+    { userID: "a-user" } 
     {
-      plugins: [
-        new StatsigAutoCapturePlugin(),
-        new StatsigSessionReplayPlugin(),
-      ],
+      plugins: [new StatsigAutoCapturePlugin(), new StatsigSessionReplayPlugin()],
     }
   );
 
   return (
     <StatsigProvider client={client} loadingComponent={<div>Loading...</div>}>
-      <App />
+      <div>Hello World</div>
     </StatsigProvider>
   );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <StatsigWrapper />
+    <App />
   </React.StrictMode>
 );
